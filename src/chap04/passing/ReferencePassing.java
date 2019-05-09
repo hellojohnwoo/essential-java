6e169d7d9ba7312a053322fed390e2a7973ba416
hellojohnwoo <hellojohnwoo@gmail.com>
2019-05-09 23:28:27 +0900

    0509 static member

---
+++ src/chap04/passing/ReferencePassing.java
@@ -0,0 +1,19 @@
+/*
+package chap04.passing;
+
+import javafx.scene.shape.Circle;
+
+public class ReferencePassing {
+    public static void main(String[] args) {
+        Circle pizza = new Circle(10);
+
+        increase(pizza);
+
+        System.out.println(pizza.radius);
+    }
+
+    static void increase(Circle m) {
+        m.radius++;
+    }
+}
+*/
